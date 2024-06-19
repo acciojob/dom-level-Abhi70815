a9ca797f2996c184e00cd9fc50df804ce25d21f1
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-19 22:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function getDOMlevel('level') {
+	const tElement=document.getElementById('level');
+	let pElement = tElement'
+		let level=0;
+	while (pElement && pElement !==document.documentElement){
+      pElement=pElement.pElement;
+		level++;
+	} 
+	alert(`The level of the element is: ${level}`);
+	{
+		
+	}
+}
+getDOMlevel("level");
